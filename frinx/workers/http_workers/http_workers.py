e5317e01c90501622a6203df970d0ea38a2d8d98
Martin Revaj <mrevaj@frinx.io>
2023-08-02 11:03:30 +0200

    fix mypy error

--- frinx/workers/http_workers/http_workers.py
+++ frinx/workers/http_workers/http_workers.py
@@ -13,7 +13,8 @@ from frinx.common.type_aliases import DictStr
 from frinx.common.type_aliases import ListAny
 from frinx.common.type_aliases import ListStr
 from frinx.common.util import cookie_jar_to_dict
-from frinx.common.util import snake_to_camel_case
+
+# from frinx.common.util import snake_to_camel_case
 from frinx.common.worker.service import ServiceWorkersImpl
 from frinx.common.worker.task_def import TaskDefinition
 from frinx.common.worker.task_def import TaskExecutionProperties
@@ -42,17 +43,19 @@ class HTTPWorkersService(ServiceWorkersImpl):
             uri: AnyHttpUrl
             method: HttpMethod
             # OPTIONAL WITH DEFAULTS
-            content_type: ContentType | None = ContentType.APPLICATION_JSON
-            connect_timeout: NonNegativeFloat | None = 360
+            content_type: ContentType = Field(default=ContentType.APPLICATION_JSON, alias='contentType')
+            connect_timeout: NonNegativeFloat = Field(default=360, alias='connectTimeout')
             headers: DictStr | None = {}
             accept: ContentType | None = None
-            read_timeout: NonNegativeFloat | None
+            read_timeout: NonNegativeFloat | None = Field(default=None, alias='readTimeout')
             body: DictAny | ListAny | str | None = None
             cookies: DictStr | None = None
 
             class Config(TaskInput.Config):
-                alias_generator = snake_to_camel_case
                 use_enum_values = True
+                # ERROR: Required dynamic aliases disallowed  [pydantic-alias]
+                # to avoid the error use static aliases instead of alias_generator
+                # alias_generator = snake_to_camel_case
 
         class WorkerOutput(TaskOutput):
             status_code: int = Field(..., alias='statusCode')
@@ -67,7 +70,6 @@ class HTTPWorkersService(ServiceWorkersImpl):
         def execute(self, worker_input: WorkerInput) -> TaskResult[Any]:
             response = http_service.http_task(worker_input)
             logs = f'{worker_input.method} {worker_input.uri} | status: {response.status_code} {response.reason}'
-            logs = f'{worker_input}'
 
             return TaskResult(
                 status=TaskResultStatus.COMPLETED if response.ok else TaskResultStatus.FAILED,
