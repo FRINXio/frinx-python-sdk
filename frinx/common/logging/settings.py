42e02a1fa36dc07bbd4eaffb2a45c27f9a73e424
MartinFoka <martin.foka@elisapolystar.com>
2024-10-16 20:50:07 +0200

    Refine LoggerConfig and LoggerSettings

--- frinx/common/logging/settings.py
+++ frinx/common/logging/settings.py
@@ -1,12 +1,14 @@
 import os
-from pathlib import Path
 
 from pydantic_settings import BaseSettings
 
 
 class LoggerSettings(BaseSettings):
-    LOG_FILE_PATH: Path = Path(os.environ.get('LOG_FILE_PATH', '/tmp/workers.log'))
-    DEFAULT_LOG_LEVEL: str = os.environ.get('LOG_LEVEL', 'INFO').upper()
-    DEFAULT_HANDLERS: list[str] = ['file']  # The root handler is used by default for logging.
+    """
+    A class that defines logger settings, which can be overridden by environment variables.
+    Defaults are provided if no environment variables are set.
+    """
+    LOG_FILE_PATH: str = os.environ.get('LOG_FILE_PATH', '/tmp/workers.log')
+    LOG_LEVEL: str = os.environ.get('LOG_LEVEL', 'INFO').upper()
     LOG_FORMAT_DEFAULT: str = '%(asctime)s | %(threadName)s | %(levelname)s | %(source)s | %(message)s'
     LOG_FORMAT_VERBOSE: str = '%(asctime)s | %(levelname)s | %(name)s | %(filename)s:%(lineno)d | %(message)s'
